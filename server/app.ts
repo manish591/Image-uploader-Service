ed8c9ec1c66ef5bd19b2edf8a6de51a620a2e937
Manish Devrani <manishdevrani777@gmail.com>
2024-02-25 14:35:44 +0530

    created global error handlers

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,21 @@
-import express from 'express';
+import express, {NextFunction, Request, Response} from 'express';
+import createError from 'http-errors';
 const app = express();
 
-app.get('/', function(req, res) {
-  res.end('Got result');
+app.get('/upload', function(req, res) {
+  res.end('File uploaded successfully');
+});
+
+// 404 route handler
+app.use(function(req: Request, res: Response, next: NextFunction) {
+  next(createError(404, '404 not found'));
+});
+
+// global error handler
+app.use(function(err: Error, req: Request, res: Response) {
+  res.status(500).json({
+    message: err.message,
+  });
 });
 
 export default app;
